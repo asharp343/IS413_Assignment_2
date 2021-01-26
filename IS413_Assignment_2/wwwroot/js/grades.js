807fb4695f8503bd1c76164d247739674ee16e09
Anthony Sharp <asharp343@gmail.com>
2021-01-25 22:52:40 -0700

    fixed jquery bug

--- IS413_Assignment_2/wwwroot/js/grades.js
+++ IS413_Assignment_2/wwwroot/js/grades.js
@@ -1,55 +1,51 @@
-﻿//function handleForm(event) { event.preventDefault(); }
+﻿$("#submitButton").click(function () {
+    let assignments = document.getElementById("assignments").value;
+    let groupProjects = document.getElementById("groupProjects").value;
+    let quizzes = document.getElementById("quizzes").value;
+    let exams = document.getElementById("exams").value;
+    let intex = document.getElementById("intex").value;
 
-$("#submitButton").click(function () {
-    assignments = $("assignments").value
-    groupProjects = $("groupProjects").value
-    quizzes = $("quizzes").value
-    exams = $("exams").value
-    intex = $("intex").value
+    let finalGradePercent = ((assignments * .5) + (groupProjects * .1) + (quizzes * .1) + (exams * .2) + (intex * .1));
 
-    alert(assignments)
-    alert(typeof(assignments))
-    
-    //finalGradePercentage = ((assignments * .5) + (groupProjects * .1) + (quizzes * .1) + (exams * .2) + (intex * .1));
+    let finalGradeLetter;
 
-    //alert(finalGradePercentage)
-    //alert(typeof(finalGradePercentage))
 
-    //if (parseInt(percentage) >= 94) {
-    //    alert("Percent: " + percentage + "\nGrade: A")
-    //}
-    //else if (parseInt(percentage) < 94 && parseInt(percentage) >= 90) {
-    //    alert("Percent: " + percentage + "\nGrade: A-")
-    //}
-    //else if (parseInt(percentage) < 90 && parseInt(percentage) >= 87) {
-    //    alert("Percent: " + percentage + "\nGrade: B+")
-    //}
-    //else if (parseInt(percentage) < 87 && parseInt(percentage) >= 84) {
-    //    alert("Percent: " + percentage + "\nGrade: B")
-    //}
-    //else if (parseInt(percentage) < 84 && parseInt(percentage) >= 80) {
-    //    alert("Percent: " + percentage + "\nGrade: B-")
-    //}
-    //else if (parseInt(percentage) < 80 && parseInt(percentage) >= 77) {
-    //    alert("Percent: " + percentage + "\nGrade: C+")
-    //}
-    //else if (parseInt(percentage) < 77 && parseInt(percentage) >= 74) {
-    //    alert("Percent: " + percentage + "\nGrade: C")
-    //}
-    //else if (parseInt(percentage) < 74 && parseInt(percentage) >= 70) {
-    //    alert("Percent: " + percentage + "\nGrade: C-")
-    //}
-    //else if (parseInt(percentage) < 70 && parseInt(percentage) >= 67) {
-    //    alert("Percent: " + percentage + "\nGrade: D+")
-    //}
-    //else if (parseInt(percentage) < 67 && parseInt(percentage) >= 64) {
-    //    alert("Percent: " + percentage + "\nGrade: D")
-    //}
-    //else if (parseInt(percentage) < 64 && parseInt(percentage) >= 60) {
-    //    alert("Percent: " + percentage + "\nGrade: D-")
-    //}
-    //else{
-    //    alert("Percent: " + percentage + "\nGrade: E")
-    //}
+    if (parseInt(finalGradePercent) >= 94) {
+        finalGradeLetter = "A";
+    }
+    else if (parseInt(finalGradePercent) < 94 && parseInt(finalGradePercent) >= 90) {
+        finalGradeLetter = "A-";
+    }
+    else if (parseInt(finalGradePercent) < 90 && parseInt(finalGradePercent) >= 87) {
+        finalGradeLetter = "B+";
+    }
+    else if (parseInt(finalGradePercent) < 87 && parseInt(finalGradePercent) >= 84) {
+        finalGradeLetter = "B";
+    }
+    else if (parseInt(finalGradePercent) < 84 && parseInt(finalGradePercent) >= 80) {
+        finalGradeLetter = "B-";
+    }
+    else if (parseInt(finalGradePercent) < 80 && parseInt(finalGradePercent) >= 77) {
+        finalGradeLetter = "C+";
+    }
+    else if (parseInt(finalGradePercent) < 77 && parseInt(finalGradePercent) >= 74) {
+        finalGradeLetter = "C";
+    }
+    else if (parseInt(finalGradePercent) < 74 && parseInt(finalGradePercent) >= 70) {
+        finalGradeLetter = "C-";
+    }
+    else if (parseInt(finalGradePercent) < 70 && parseInt(finalGradePercent) >= 67) {
+        finalGradeLetter = "D+";
+    }
+    else if (parseInt(finalGradePercent) < 67 && parseInt(finalGradePercent) >= 64) {
+        finalGradeLetter = "D";
+    }
+    else if (parseInt(finalGradePercent) < 64 && parseInt(finalGradePercent) >= 60) {
+        finalGradeLetter = "D-";
+    }
+    else {
+        finalGradeLetter = "E";
+    }
 
+    alert("Percent: " + finalGradePercent + "\nGrade: " + finalGradeLetter);
 });
